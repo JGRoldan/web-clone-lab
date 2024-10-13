a7a0b50d5b1dc968d82d9f2e22f931c561b42df5
JGRDeveloper <jgroldan.dev@gmail.com>
2024-10-13 09:46:18 -0300

    Shop page terminada.

---
+++ kultur5-clone/src/data/imagesData.js
@@ -0,0 +1,58 @@
+export const imageData = [
+    {
+        id: 1,
+        title: "Waves Ring",
+        srcImg: "WAVES-RING-scaled.webp",
+        srcImgHover: "WAVES-HAND-WEB-scaled.webp",
+        price: '210,00'
+    },
+    {
+        id: 2,
+        title: "Bracelet",
+        srcImg: "BRACELET-PRODUCT-PHOTO.webp",
+        srcImgHover: "forweb14-scaled.webp",
+        price: '310,00'
+    },
+    {
+        id: 3,
+        title: "Love Necklace",
+        srcImg: "lovenecklace.webp",
+        srcImgHover: "love_necklace_low_q_3.webp",
+        price: '360,00'
+    },
+    {
+        id: 4,
+        title: "Love Ring",
+        srcImg: "love_signet_ring_model_2.jpg",
+        srcImgHover: "lovering.webp",
+        price: '250,00'
+    },
+    {
+        id: 5,
+        title: "Knot Ring",
+        srcImg: "knot_ring_model_23cf483a-d548-4e17-903c-688b2a98096b.jpg",
+        srcImgHover: "knotring.webp",
+        price: '230,00'
+    },
+    {
+        id: 6,
+        title: "Rope Earrings",
+        srcImg: "ROPE-EARRINGS-2-scaled.webp",
+        srcImgHover: "ROPE-EARRINGS-3-2-scaled.webp",
+        price: '290,00'
+    },
+    {
+        id: 7,
+        title: "Symbol Bracelet",
+        srcImg: "symbolbracelet2.webp",
+        srcImgHover: "symbol_necklace_final.jpg",
+        price: '250,00'
+    },
+    {
+        id: 8,
+        title: "Buckled thin Ring",
+        srcImg: "Buckledthinring.webp",
+        srcImgHover: "buckled_ring_thin_model.jpg",
+        price: '230,00'
+    }
+]
