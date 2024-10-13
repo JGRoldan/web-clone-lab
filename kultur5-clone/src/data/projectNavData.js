376f09328fb368a85251234db9c65ae7370a09ae
JGRDeveloper <jgroldan.dev@gmail.com>
2024-10-13 10:24:12 -0300

    Kultur5 terminado.

---
+++ kultur5-clone/src/data/projectNavData.js
@@ -0,0 +1,27 @@
+export const projectNav = [
+    {
+        id: 1,
+        title: '/ Raspberry Collision',
+        imgSrc: 'text_front_page_3-min.webp'
+    },
+    {
+        id: 2,
+        title: '/ Flower Shop',
+        imgSrc: 'R1-04255-0024-scaled.webp'
+    },
+    {
+        id: 3,
+        title: '/ Gothenburg',
+        imgSrc: 'Kultur5_117-min.webp'
+    },
+    {
+        id: 4,
+        title: '/ Workshop Documentation',
+        imgSrc: 'Kultur5_jacob_354-min-min.webp'
+    },
+    {
+        id: 5,
+        title: '/ Defining Kultur 5',
+        imgSrc: 'BESKUREN17-min.webp'
+    },
+]
